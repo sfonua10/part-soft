eab4610ae389235cd405e3736f93e8a99b1b7bac
Saia Fonua <saiafonua@gmail.com>
2023-08-29 04:23:23 -0600

    remove isactive checkboxes

--- src/components/VendorForm2.jsx
+++ src/components/VendorForm2.jsx
@@ -1,26 +1,23 @@
 'use client'
-import { useLayoutEffect, useRef, useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { mutate } from 'swr'
 import AddVendorSliderOver from './AddVendorSlideOver'
 
 export default function VendorForm2({ data }) {
-  const checkbox = useRef()
   const [open, setOpen] = useState(false)
-  const [checked, setChecked] = useState(false)
-  const [indeterminate, setIndeterminate] = useState(false)
   const [selectedPeople, setSelectedPeople] = useState(data || [])
   const [selectedVendor, setSelectedVendor] = useState({})
-console.log('selectedPeople', selectedPeople)
+  console.log('selectedPeople', selectedPeople)
   const addNewVendor = () => {
     setSelectedVendor({})
     setOpen(true)
   }
   useEffect(() => {
     if (data) {
-      setSelectedPeople(data);
+      setSelectedPeople(data)
     }
-  }, [data]);
-  
+  }, [data])
+
   const deleteVendor = async (vendorToDelete) => {
     try {
       const response = await fetch('/api/delete-vendor', {
@@ -47,32 +44,6 @@ console.log('selectedPeople', selectedPeople)
     }
   }
 
-  // useLayoutEffect(() => {
-  //   const isIndeterminate =
-  //     selectedPeople?.length > 0 && selectedPeople?.length < data?.length
-  //   setChecked(selectedPeople?.length === data?.length)
-  //   setIndeterminate(isIndeterminate)
-  //   checkbox.current.indeterminate = isIndeterminate
-  // }, [selectedPeople])
-
-  // function toggleAll() {
-  //   setSelectedPeople(checked || indeterminate ? [] : data)
-  //   setChecked(!checked && !indeterminate)
-  //   setIndeterminate(false)
-  // }
-
-  const toggleActiveStatus = (vendorId) => {
-      // Toggle the vendor's isActive status
-      const updatedPeople = selectedPeople.map(vendor => 
-        vendor._id === vendorId ? { ...vendor, isActive: !vendor.isActive } : vendor
-      );
-    
-      // Update the local state
-      setSelectedPeople(updatedPeople);
-  
-      // Send the updated vendor to the database
-      // updateVendorInDatabase(updatedVendor);
-  }
   return (
     <>
       <div className="px-4 sm:px-6 lg:px-8">
@@ -100,34 +71,9 @@ console.log('selectedPeople', selectedPeople)
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
               <div className="relative">
-                {/* {selectedPeople?.length > 0 && (
-                  <div className="absolute left-14 top-0 flex h-12 items-center space-x-3 bg-white sm:left-12">
-                    <button
-                      type="button"
-                      className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                    >
-                      Bulk edit
-                    </button>
-                    <button
-                      type="button"
-                      className="inline-flex items-center rounded bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                    >
-                      Delete all
-                    </button>
-                  </div>
-                )} */}
                 <table className="min-w-full table-fixed divide-y divide-gray-300">
                   <thead>
                     <tr>
-                      <th scope="col" className="relative px-7 sm:w-12 sm:px-6 invisible"> {/**TODO: will make visible later */}
-                        {/* <input
-                          type="checkbox"
-                          className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          ref={checkbox}
-                          checked={checked}
-                          // onChange={toggleAll}
-                        /> */}
-                      </th>
                       <th
                         scope="col"
                         className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
@@ -182,27 +128,6 @@ console.log('selectedPeople', selectedPeople)
                             : undefined
                         }
                       >
-                        <td className="relative px-7 sm:w-12 sm:px-6">
-                          {selectedPeople?.includes(vendor) && (
-                            <div className="absolute inset-y-0 left-0 w-0.5 bg-indigo-600" />
-                          )}
-                          <input
-                            type="checkbox"
-                            className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                            value={vendor?.isActive}
-                            checked={vendor.isActive}
-                            onChange={() => toggleActiveStatus(vendor._id)}
-
-                          />
-                        </td>
-                        {/* <td
-                        className={classNames(
-                          'whitespace-nowrap py-4 pr-3 text-sm font-medium',
-                          selectedPeople?.includes(vendor) ? 'text-indigo-600' : 'text-gray-900'
-                        )}
-                      >
-                        {vendor?.name}
-                      </td> */}
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
                           {vendor.name}
                         </td>
