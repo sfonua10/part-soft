003f78dea6be2cd6af42874717a6613806b7634b
Saia Fonua <saiafonua@gmail.com>
2023-09-14 23:06:19 -0600

    comment out gmail.com

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -54,8 +54,8 @@ const handler = NextAuth({
 
         // Restrict access based on the email domain
         if (account.provider === 'google') {
-          // if (!profile.email_verified || !profile.email.endsWith("@uniteddieselservice.net")) {
-          if (!profile.email_verified || !profile.email.endsWith("@gmail.com")) {
+          if (!profile.email_verified || !profile.email.endsWith("@uniteddieselservice.net")) {
+          // if (!profile.email_verified || !profile.email.endsWith("@gmail.com")) {
             console.error('Invalid email domain or email not verified');
             return false;
           }
