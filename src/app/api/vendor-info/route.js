ab0fabd6e3a00b3a878e7ef6785efd45849073c5
Saia Fonua <saiafonua@gmail.com>
2023-09-29 12:24:51 -0600

    remove active only from request

--- src/app/api/vendor-info/route.js
+++ src/app/api/vendor-info/route.js
@@ -1,25 +1,40 @@
+// import Vendor from "@/models/vendor";
+
+// export async function GET(request) {
+//     try {
+//       //Get the activeOnly query parameter from the request
+//       const url = new URL(request?.url);
+//       const activeOnly = url.searchParams.get('activeOnly');
+
+//       let query = {};
+
+//       if (activeOnly === 'true') {
+//         query.isActive = true;
+//       }
+
+//       const vendors = await Vendor.find(query);
+      
+//       return new Response(JSON.stringify(vendors), { status: 200 });
+//     } catch (error) {
+//       console.error("Error fetching vendors:", error.message);
+//       return new Response(JSON.stringify({ message: error.message }), {
+//         status: 500,
+//       });
+//     }
+//   }
+  
 import Vendor from "@/models/vendor";
 
-export async function GET(request) {
+export async function GET() {
     try {
-      //Get the activeOnly query parameter from the request
-      const url = new URL(request?.url);
-      const activeOnly = url.searchParams.get('activeOnly');
-
-      let query = {};
-
-      if (activeOnly === 'true') {
-        query.isActive = true;
-      }
-
-      const vendors = await Vendor.find(query);
+        // Since you're not using activeOnly, you can query all vendors directly
+        const vendors = await Vendor.find();
       
-      return new Response(JSON.stringify(vendors), { status: 200 });
+        return new Response(JSON.stringify(vendors), { status: 200 });
     } catch (error) {
-      console.error("Error fetching vendors:", error.message);
-      return new Response(JSON.stringify({ message: error.message }), {
-        status: 500,
-      });
+        console.error("Error fetching vendors:", error.message);
+        return new Response(JSON.stringify({ message: error.message }), {
+            status: 500,
+        });
     }
-  }
-  
+}
