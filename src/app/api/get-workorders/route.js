2517f7a8b68863fc0894de5883d50569947f77a4
Saia Fonua <saiafonua@gmail.com>
2023-12-08 21:02:28 -0700

    hotfix 1

--- src/app/api/get-workorders/route.js
+++ src/app/api/get-workorders/route.js
@@ -3,11 +3,11 @@ import User from '@/models/user'
 
 export async function GET(request) {
   try {
-    // Get the user ID and work order ID from query parameters
     const url = new URL(request.url);
     const userId = url.searchParams.get('userId');
     const workOrderId = url.searchParams.get('workOrderId');
 
+    // Validate userId early to avoid unnecessary database calls
     if (!userId) {
       return new Response(JSON.stringify({ message: 'User ID is required' }), {
         status: 400,
@@ -15,24 +15,24 @@ export async function GET(request) {
       });
     }
 
-    // Fetch the user's details from the database
-    const user = await User.findById(userId);
+    // Use Promise.all to run both queries in parallel when both userId and workOrderId are provided
+    // This reduces waiting time when both user and workOrder are needed
+    const [user, workOrder] = await Promise.all([
+      User.findById(userId),
+      workOrderId ? WorkOrder.findOne({ workOrderNumber: workOrderId }) : null,
+    ]);
+
+    // Check user existence
     if (!user) {
       return new Response(JSON.stringify({ message: 'User not found' }), {
         status: 404,
         headers: { 'Content-Type': 'application/json' },
       });
     }
-    const organizationId = user.organizationId;
 
-    // If a specific workOrderId is provided, fetch that work order
+    // If workOrderId is provided, validate the workOrder
     if (workOrderId) {
-      const workOrder = await WorkOrder.findOne({
-        workOrderNumber: workOrderId,
-        organizationId: organizationId,  // Ensuring the work order belongs to the user's organization
-      });
-
-      if (!workOrder) {
+      if (!workOrder || workOrder.organizationId !== user.organizationId) {
         return new Response(JSON.stringify({ message: 'Work order not found' }), {
           status: 404,
           headers: { 'Content-Type': 'application/json' },
@@ -44,17 +44,17 @@ export async function GET(request) {
         headers: { 'Content-Type': 'application/json' },
       });
     }
-    // If no specific workOrderId is provided, fetch all work orders associated with the user's organization
-    else {
-      const workOrders = await WorkOrder.find({
-        organizationId: organizationId,  // Filtering by organization ID
-      });
 
-      return new Response(JSON.stringify(workOrders), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+    // Fetch all work orders if no specific workOrderId is provided
+    const workOrders = await WorkOrder.find({
+      organizationId: user.organizationId,
+    });
+
+    return new Response(JSON.stringify(workOrders), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
   } catch (error) {
     console.error('Error fetching work orders:', error.message);
     return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
